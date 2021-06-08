Fixing a bug concerning the function `cal_gamma_R`: Since `cal_gamma_R` has been decorated with `_CorrectIftAtPaddedBoundary`, in order to correct the calculation of the interface tension of single planar interfaces in systems with boundary condition 'pad', the function did not accept positional arguments but ony keyword arguments. This has been changed back in favor for the positional arguments.

code/ldft_classes_v2/lg_2d_highl.py
@@ -811,7 +811,7 @@ class LG2dAOHighl(LdftModel):
             pass
 
         def __call__(self, func):
-            def funcWraper(self, **kwargs):
+            def funcWraper(self, arg=None):
                 if self._bound_cond == 'pad':
                     Phi_id = lambda r: r*(np.log(r)-1)
                     Phi_0 = self._cal_Phi_0
@@ -830,7 +830,7 @@ class LG2dAOHighl(LdftModel):
                 else:
                     x=0
                     #print('else x='+str(x))
-                return func(self, **kwargs)+x
+                return func(self, arg)+x
             return funcWraper
 
     @_CorrectIftAtPaddedBoundary()